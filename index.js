d1c6d5e21d70e1e559d7fa018c7e9edcc620e3e3
SmackSmash <danmoffat@gmail.com>
2019-08-02 17:35:37 +0100

    Refactored code, added routes and services folders and moved code out to them

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      console.log('Access Token', accessToken);
-      console.log('Refresh Token', refreshToken);
-      console.log('Profile', profile);
-    }
-  )
-);
-
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/auth/google' }),
-  (req, res) => {
-    res.redirect('/');
-  }
-);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
