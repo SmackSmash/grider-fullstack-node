dfaab57f97f886d02565f35eac2960d2c35a1bbc
SmackSmash <danmoffat@gmail.com>
2019-08-02 15:35:52 +0100

    Prepared project for Heroku

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello there!');
+  res.send({ hi: 'there' });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
