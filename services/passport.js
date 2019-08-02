0aa7838c94a6e1ae6498a5d4cd5556c71554ff12
SmackSmash <danmoffat@gmail.com>
2019-08-02 21:47:15 +0100

    Completed basic oauth implementation plus cookies

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +21,20 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, cb) => {
-      console.log('Access Token', accessToken);
-      console.log('Refresh Token', refreshToken);
-      console.log('Profile', profile);
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (!existingUser) {
+        try {
+          const user = new User({ googleId: profile.id });
+          await user.save();
+          done(null, user);
+        } catch (err) {
+          console.error(err);
+        }
+        return console.log('User created!');
+      }
+      done(null, existingUser);
+      console.log('User already exists!');
     }
   )
 );
