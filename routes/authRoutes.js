d1796f9d48d4cfb3f455fe37a2fc4e2d6c58cc0b
SmackSmash <danmoffat@gmail.com>
2019-08-03 14:46:07 +0100

    Trying to fix error with prod build login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,13 +12,15 @@ module.exports = app => {
     res.send(req.user || 'Logged out successfully');
   });
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/auth/google' }),
-    (req, res) => {
-      res.redirect('/');
-    }
-  );
+  // app.get(
+  //   '/auth/google/callback',
+  //   passport.authenticate('google', { failureRedirect: '/auth/google' }),
+  //   (req, res) => {
+  //     res.redirect('/');
+  //   }
+  // );
+
+  app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }));
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user || 'No user signed in');
