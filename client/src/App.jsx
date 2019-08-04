9d487839ec7c13762f64b98e88408c15253c1a16
SmackSmash <danmoffat@gmail.com>
2019-08-04 14:41:48 +0100

    Added proxy config for client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = props => {
-  return <div>App works!</div>;
+  return (
+    <div className='app'>
+      <a href='/auth/google'>Sign in with google</a>
+    </div>
+  );
 };
 
 export default App;
