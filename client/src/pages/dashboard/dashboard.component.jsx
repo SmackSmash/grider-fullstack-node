80ec9c95b26e0f49dfb3e628e6f8bfe37a4caac4
SmackSmash <danmoffat@gmail.com>
2019-08-05 14:56:28 +0100

    Stripe API keys and environment variables

--- client/src/pages/dashboard/dashboard.component.jsx
+++ client/src/pages/dashboard/dashboard.component.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './dashboard.styles.scss';
 
 const Dashboard = props => {
-  return <div>Dashboard Works!</div>;
+  return (
+    <div>
+      Dashboard Works!<p>{process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}</p>
+    </div>
+  );
 };
 
 export default Dashboard;
