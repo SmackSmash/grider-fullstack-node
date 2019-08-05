3349b687127553e9812e6037e0fa7373e8722fe1
SmackSmash <danmoffat@gmail.com>
2019-08-05 18:32:01 +0100

    Fixed issue in deployment where default route was set in express

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,6 @@
 const passport = require('passport');
 
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.send('Routes work!');
-  });
-
   app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
   app.get('/api/logout', (req, res) => {
